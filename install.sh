b9daf7d53b1e9fee5d4feb0668e34e2caffcde0e
Arvet Fuchs <e-itsinsext2@geberit.com>
2022-03-17 11:15:32 +0100

    feat[install]: prepared install script

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-yum install -y python3-pip bind-utils
+# redhat
+  # yum install -y python3-pip bind-utils
+
+# archlinux / manjaro
+  # pacman -S python-pip bind
 
 cp ./bind-api.service /etc/systemd/system/
+
 cp ./bind-api.conf /etc/
+echo "^ set / control permissions"
+# ^ permission set correct! --> ???
+  # chown root:root /etc/bind-api.conf
+  # chmod 640 /etc/bind-api.conf
+
 cp ./bind-restapi.py /usr/local/bin/
+
+echo "^ set / control permissions"
+# ^ permission set correct! --> ???
+  # chown root:root /usr/local/bin/bind-restapi.py
+  # chmod 700 /usr/local/bin/bind-restapi.py
+
 pip3 install -r requirements.txt
 systemctl enable bind-api.service
 systemctl start bind-api.service
